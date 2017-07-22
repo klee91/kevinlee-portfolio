a369cdc75d79a195281119b4934883504ce91291
Kevin Lee <lee.kev1991@gmail.com>
2017-07-22 13:08:50 -0400

    jquery

--- public/js/email.js
+++ public/js/email.js
@@ -2,7 +2,7 @@ var $contactForm = $('#emailForm');
 $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: '//formspree.io/lee.kev1991@email.com',
+		url: '//formspree.io/lee.kev1991@gmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
@@ -15,7 +15,7 @@ $contactForm.submit(function(e) {
 		},
 		error: function(err) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+			$contactForm.append('<div class="alert alert--error">Oops, there was an error.</div>');
 		}
 	});
 });
